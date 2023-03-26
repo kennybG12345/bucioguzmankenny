d9a111c736c0244bed75036ad41f810312968e58
kennybG12345 <126514190+kennybG12345@users.noreply.github.com>
2023-03-25 21:44:01 -0600

    Add files via upload

---
+++ M1/S3/parcial 1/Java/04 arreglos.java
@@ -0,0 +1,18 @@
+//PRACTICA #4
+import java.io.*;
+class Vector
+{
+    public static void main(String[] args)
+    {
+        int[] numeros = new int [3];
+        numeros[0] = 5;
+        numeros[1] = 9;
+        numeros[2] = 2;
+        System.out.print("El primer elemento del arreglo es: "+numeros[0]);
+        System.out.print("El primer elemento del arreglo es: "+numeros[1]);
+        System.out.print("El primer elemento del arreglo es: "+numeros[2]);
+
+
+
+    }
+}
