c2a36e32589a591e6dac9b0080f4c5819c78c3cf
kennybG12345 <126514190+kennybG12345@users.noreply.github.com>
2023-03-27 12:23:00 -0600

    Add files via upload

---
+++ M1/S3/parcial 2/java/ForArreglos,estaturas.java
@@ -0,0 +1,10 @@
+import java.io.*;
+class Estaturas
+{
+    public static void main(String[] args)
+    {
+        float[] estaturas = {1.8f, 1.5f, 2.0f};
+        for(int i = 0; i <= 2; i++)
+            System.out.println(estaturas[i]);
+    }
+}
